328b8e09bc3c41b534a1039e61bd23d538de9408
Ali Kooshesh <alikooshesh007@gmail.com>
2024-06-21 11:02:10 +0330

    major-change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+'use client'
+
 import Image from "next/image";
+import {fetchEventSource} from "@microsoft/fetch-event-source";
+import {useEffect, useState} from "react";
 
 export default function Home() {
+
   return (
-    <></>
+    <>
+      <p>
+      </p>
+    </>
   );
 }
