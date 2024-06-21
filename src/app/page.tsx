18341d80d9d861d49c35652e036cb9942c4c973a
Ali Kooshesh <alikooshesh007@gmail.com>
2024-06-21 15:49:39 +0330

    chore/major-changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
 
   return (
     <>
-      <Link href={"dashboard/translator"}>
+      <Link href={"dashboard/translate"}>
           translator
       </Link>
     </>
